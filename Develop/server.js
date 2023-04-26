419f9a523186c2bf1d1d7e175d5312c6af846e38
erikchiodo <erikchiodo@gmail.com>
2023-04-25 20:36:29 -0400

    Updated Get Route for Main Page and Notes Page. Created Routes Folder

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// MiddleWare
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("./public/index.html"));
+
+// Get Route for Main Page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// Get Route for Notes Page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
