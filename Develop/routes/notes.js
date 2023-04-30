77906f51e09a4f0eaeb97fcdf9b573b1615d90f0
erikchiodo <erikchiodo@gmail.com>
2023-04-30 19:03:43 -0400

    Cleaned up notes.js by removing comments and updated test data

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,47 +1,43 @@
 const notes = require("express").Router();
 const uuid = require("../helpers/uuid.js");
 const fs = require("fs");
-const noteData = require("/db/notes.json");
 
 // Get Route for Notes Data
 notes.get("/", (req, res) => {
-// TODO: pull information from note.json file
-// TODO: serialize data from notes.json
-// TODO: present information on html
   try {
-    console.log(notesData);
-    return res.json(noteData);
+    const notes = JSON.parse(fs.readFileSync("db/notes.json", (encoding = "utf-8")));
+    return res.json(notes);
   }
   catch {
     error.json()
   }
 });
 
-// Post Route for Notes Data
+// Post request to add new note
 notes.post("/", (req, res) => {
-  // TODO: Collect inputs from HTML request
-  const { title, text, notesId } = req.body;
+  const { title, text } = req.body;
 
-  if (title && text && notesId) {
+  if (title && text) {
     const newPost = {
       title,
       text,
-      notesId: uuid(),
+      id: uuid(),
     };
-
-    // TODO: Stringify (deserialize) inputs to be written to notes.json
-    const request = {
-      method: "POST",
-      header: { "Content-Type": "application/json" },
-      body: JSON.stringify(newPost),
-    };
-
-    // TODO: Write notes to notes.json
-    fs.writeFile("/db/notes.json", request, { flag: "a" }, (err) => {
-      if (err) throw err;
-      console.log("New note added to notes.json");
-    });
+    const notes = JSON.parse(
+      fs.readFileSync("db/notes.json", (encoding = "utf-8"))
+    );
+    notes.push(newPost);
+    fs.writeFileSync("db/notes.json", JSON.stringify(notes));
+    return res.json(newPost);
   };
+});
+
+// Delete request to delete note (by id)
+notes.delete("/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync("db/notes.json", (encoding = "utf-8")));
+  const updatedNotes = notes.filter((note) => note.id != req.params.id);
+  fs.writeFileSync("db/notes.json", JSON.stringify(updatedNotes));
+  return res.json({ok:true});
 });
 
 module.exports = notes;
